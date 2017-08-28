aaa66450cbac30d62082c386b53ba787acf55fbd
infinityAlive <sonic@ehanlin.com.tw>
2017-08-28 11:54:20 +0800

    release 0.0.2-SNAPSHOT

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 test="http://test.ehanlin.com.tw/event/api/Deploy"
 production="http://www.ehanlin.com.tw/event/api/Deploy"
-repository="{\"Repository\":\"event-award\",\"Tag\":\"${TRAVIS_TAG}\",\"Owner\":\"eHanlin\",\"Password\":\"${EHANLIN_PW}\",\"Name\":\"event-award\"}"
+repository="{\"Repository\":\"event-award\",\"Tag\":\"${TRAVIS_TAG}\",\"Owner\":\"eHanlin\",\"Password\":\"${EHANLIN_PW}\",\"Name\":\"award\"}"
 contentTypeJson="Content-Type: application/json"
 
 case "${TRAVIS_TAG}" in
