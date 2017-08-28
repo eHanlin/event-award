8f579149833ec6f4d84cba4da581eecd31220ede
infinityAlive <sonic@ehanlin.com.tw>
2017-08-28 17:15:29 +0800

    補上 FB 分享連結功能

--- dist/js/award.js
+++ dist/js/award.js
@@ -1,1 +1,1 @@
-$(function(){$("#award-application").hide(),$("#award-standard").hide(),$("#sweet-tip").hide()});
+$(function(){$("#award-application").hide(),$("#award-standard").hide(),$("#sweet-tip").hide(),$(".work-hard").on("click",function(){window.open("https://www.facebook.com/sharer/sharer.php?u="+encodeURIComponent(location.href),"facebook-share-dialog","width=626,height=436")})});
--- src/js/award.js
+++ src/js/award.js
@@ -2,4 +2,12 @@ $(function() {
   $("#award-application").hide();
   $("#award-standard").hide();
   $("#sweet-tip").hide();
+  $(".work-hard").on("click", function() {
+    window.open(
+      "https://www.facebook.com/sharer/sharer.php?u=" +
+        encodeURIComponent(location.href),
+      "facebook-share-dialog",
+      "width=626,height=436"
+    );
+  });
 });
